b4043418738f54a8e51dd4cf1343b87a87416c71
Ala Ben Daoud <alabendawed@gmail.com>
2025-10-07 22:27:08 +0100

    Button like et acheter avec barre de recherche

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { ListEventComponent } from './layout/list-event/list-event.component';
+import { NotFoundComponent } from './layout/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ { path: 'home', component: HomeComponent },
+  {
+    path: 'list',
+    component: ListEventComponent,
+    
+  },
+  { path: '**', component: NotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
